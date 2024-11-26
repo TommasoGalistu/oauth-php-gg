9ae8b090969dc3a2d91c457abf289a5ce6bc5988
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-11-26 16:49:29 +0100

    feat: google access

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Su VsCode ho creato la struttura del progetto con i vari style
 
 Ho importato le variabili dal file env e ho aperto la sessione
 
+Ho scritto il codice gestisce l'autenticazione dell'utente tramite Google. Quando l'utente viene reindirizzato alla pagina con un parametro code (fornito da Google dopo l'autenticazione), il codice utilizza questo code per richiedere un access token. Questo token è una chiave temporanea che consente all'app di accedere ai dati dell'utente.
+
+Dopo aver ricevuto il token, il codice invia un'altra richiesta per ottenere le informazioni dell'utente, come nome ed e-mail. Se queste informazioni vengono recuperate correttamente, vengono salvate nella sessione dell'utente per mantenerlo autenticato. Infine, l'utente viene reindirizzato alla pagina del profilo.
+
+Se non è presente un parametro code, l'utente viene inviato alla pagina di login di Google per autenticarsi.
+
--- google-oauth.php
+++ google-oauth.php
@@ -4,6 +4,36 @@ require_once __DIR__ . '/.env.php';
 
 session_start();
 
-
+// If the captured code param exists and is valid
+if (isset($_GET['code']) && !empty($_GET['code'])) {
+    // Execute cURL request to retrieve the access token
+    $params = [
+        'code' => $_GET['code'],
+        'client_id' => $google_oauth_client_id,
+        'client_secret' => $google_oauth_client_secret,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'grant_type' => 'authorization_code'
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://accounts.google.com/o/oauth2/token');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response, true);
+} else {
+    // Define params and redirect to Google Authentication page
+    $params = [
+        'response_type' => 'code',
+        'client_id' => $google_oauth_client_id,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'scope' => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+        'access_type' => 'offline',
+        'prompt' => 'consent'
+    ];
+    header('Location: https://accounts.google.com/o/oauth2/auth?' . http_build_query($params));
+    exit;
+}
 
 ?>
